b26a826ff848621c849440933df8282a5dcdfc55
samiul <samiul@digitalaptech.com>
2018-11-28 19:37:23 +0530

    drf docs tuto complete

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 from .views import SnippetViewSet, UserViewSet
 from . import views
+
+schema_view = get_schema_view(title='Pastebin API')
 
 snippet_list = SnippetViewSet.as_view({'get': 'list', 'post': 'create'})
 snippet_detail = SnippetViewSet.as_view({
@@ -22,6 +25,6 @@ router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('schema/', schema_view),
 ]
-
